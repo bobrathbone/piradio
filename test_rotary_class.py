28ca7c7100c2d97d473dbee041f28531ebfbdace
bobrathbone <basil123>
2014-07-31 08:53:23 +0200

    Test programs improved

--- test_rotary_class.py
+++ test_rotary_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Raspberry Pi Rotary Test Encoder Class
-# $Id: test_rotary_class.py,v 1.2 2014/03/25 19:42:00 bob Exp $
+# $Id: test_rotary_class.py,v 1.4 2014/07/22 13:10:01 bob Exp $
 #
 # Author : Bob Rathbone
 # Site   : http://www.bobrathbone.com
@@ -16,7 +16,12 @@
 
 import sys
 import time
+import atexit
+import traceback
+
 from rotary_class import RotaryEncoder
+
+stderr = sys.stderr.write;
 
 # Switch definitions
 MENU_SWITCH = 25
@@ -26,26 +31,55 @@ UP_SWITCH = 17
 DOWN_SWITCH = 18
 MUTE_SWITCH = 4 
 
+# Try to trap any exit errors 
+def exit_fn():
+        if not traceback.format_exc().startswith('None'):
+                s=traceback.format_exc()
+	sys.exit(0)
 
-# This is the event callback routine to handle events
-def callback(event):
-	if event == RotaryEncoder.CLOCKWISE:
-		print "Clockwise", RotaryEncoder.CLOCKWISE
-	elif event == RotaryEncoder.ANTICLOCKWISE:
-		print "Anticlockwise", RotaryEncoder.BUTTONDOWN
-	elif event == RotaryEncoder.BUTTONDOWN:
-		print "Button down", RotaryEncoder.BUTTONDOWN
-	elif event == RotaryEncoder.BUTTONUP:
-		print "Button up", RotaryEncoder.BUTTONUP
+# Register
+atexit.register(exit_fn)
+
+# This is the callback routine to handle volume events
+def volume_event(event):
+	display_event("Volume", event)
 	return
 
-#rswitch = RotaryEncoder(PIN_A,PIN_B,BUTTON,callback)
-volumeknob = RotaryEncoder(LEFT_SWITCH,RIGHT_SWITCH,MUTE_SWITCH,callback)
-tunerknob = RotaryEncoder(UP_SWITCH,DOWN_SWITCH,MENU_SWITCH,callback)
+# This is the callback routine to handle tuner events
+def tuner_event(event):
+	display_event("Tuner", event)
+	return
 
+def display_event(name,event):
+	if event == RotaryEncoder.CLOCKWISE:
+		print name + " clockwise", RotaryEncoder.CLOCKWISE
+	elif event == RotaryEncoder.ANTICLOCKWISE:
+		print name + " anticlockwise", RotaryEncoder.BUTTONDOWN
+	elif event == RotaryEncoder.BUTTONDOWN:
+		print name + " button down", RotaryEncoder.BUTTONDOWN
+	elif event == RotaryEncoder.BUTTONUP:
+		print name + " button up", RotaryEncoder.BUTTONUP
+	return
+
+# Allow user to select board revision
+revision = 2
+stderr("Are you using an old revision 1 board y/n: ")
+answer = raw_input("")
+if answer == 'y':
+	revision = 1
+
+volumeknob = RotaryEncoder(LEFT_SWITCH,RIGHT_SWITCH,MUTE_SWITCH,volume_event,revision)
+tunerknob = RotaryEncoder(UP_SWITCH,DOWN_SWITCH,MENU_SWITCH,tuner_event,revision)
+
+print "Use Ctl-C to exit"
 
 while True:
-	time.sleep(0.5)
+	try:
+		time.sleep(0.2)
+	except KeyboardInterrupt:
+		print "\nExit"
+		sys.exit(0)
+
 
 
 
