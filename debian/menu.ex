b4d3d16ff9897ed32492d23ccd2e1d7137fa0672
Tobias Schlemmer <keinstein@users.sourcforge.net>
2014-10-03 20:49:23 +0200

    Plain result of dh_make

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(piradio-3.13):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="piradio-3.13" command="/usr/bin/piradio-3.13"
---
+++ debian/piradio-3.13.default.ex
@@ -0,0 +1,10 @@
+# Defaults for piradio-3.13 initscript
+# sourced by /etc/init.d/piradio-3.13
+# installed at /etc/default/piradio-3.13 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
